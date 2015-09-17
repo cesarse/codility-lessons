76563df7d9c2b7cf2928080ec1740ac21dd701a8
César Seragiotto <cesarse@icloud.com>
2015-09-17 16:08:13 -0300

    Solution for Tape Equilibrium task from lesson 1.

---
+++ codility-lessons/src/test/java/cesarse/codility/lesson1/TapeEquilibrium/SolutionTest.java
@@ -0,0 +1,48 @@
+package cesarse.codility.lesson1.TapeEquilibrium;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import cesarse.codility.lesson1.TapeEquilibrium.Solution;
+
+public class SolutionTest {
+
+	@Test
+	public void test1() {
+		int[] A = {3, 1, 2, 4, 3};
+		int expected = 1;
+		Solution s = new Solution();
+		assertEquals(expected, s.solution(A));
+		assertEquals(expected, s.unintelligibleSolution(A));
+	}
+
+	@Test
+	public void test2() {
+		int[] A = {1, -1, 1, -1, 1, -1};
+		int expected = 0;
+		Solution s = new Solution();
+		assertEquals(expected, s.solution(A));
+		assertEquals(expected, s.unintelligibleSolution(A));
+	}
+	
+	@Test
+	public void test3() {
+		int[] A = {0, 0, 0, 0, 0, 0};
+		int expected = 0;
+		Solution s = new Solution();
+		assertEquals(expected, s.solution(A));
+		assertEquals(expected, s.unintelligibleSolution(A));
+	}
+
+	@Test
+	public void test4() {
+		int[] A = {-2, 3};
+		int expected = 5;
+		Solution s = new Solution();
+		assertEquals(expected, s.solution(A));
+		assertEquals(expected, s.unintelligibleSolution(A));
+	}
+	
+}
+
